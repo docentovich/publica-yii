cf5088a38417a571af7be51f2c4373b7399e883d
Snail <>
2017-07-05 03:59:06 +0300

    mailer

--- app/htdocs/environments/prod/common/config/main-local.php
+++ app/htdocs/environments/prod/common/config/main-local.php
@@ -11,6 +11,22 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.yandex.ru',
+                'username' => 'publica.mail@yandex.ru',
+                'password' => 'avbva007',
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],
+        ],
+    ],
+    'modules' => [
+        'user' => [
+            'mailer' => [
+                'sender' => ['publica.mail@yandex.ru' => 'Publica']
+            ],
         ],
     ],
 ];
