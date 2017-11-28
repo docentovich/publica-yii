cd8a415157ebc1fe49bc49eebfa46ba634e4f6d7
docentovich <andrei.prozorov@mail.ru>
2017-11-28 14:11:27 +0300

    чистка мусора

--- app/htdocs/templates/frontend/tosee_probank/layouts/parts/footer.php
+++ app/htdocs/templates/frontend/tosee_probank/layouts/parts/footer.php
@@ -5,13 +5,19 @@
             <div class="col-xs-24">
                 <div class="footer__footer-inner">
                     <div class="footer__logo footer__logo_tosee">
-                        <i class="footer__i sprite-img sp-sprite sp-sprite__tosee-grey"></i>
+                        <a href="http://tosee.shablonkin.shn-host.ru/">
+                            <i class="footer__i sprite-img sp-sprite sp-sprite__tosee-grey"></i>
+                        </a>
                     </div>
                     <div class="footer__logo footer__logo_probank">
-                        <i class="footer__i sprite-img sp-sprite sp-sprite__probank-grey"></i>
+                        <a href="http://probank.shablonkin.shn-host.ru/">
+                            <i class="footer__i sprite-img sp-sprite sp-sprite__probank-grey"></i>
+                        </a>
                     </div>
                     <div class="footer__logo footer__logo_shotme">
-                        <i class="footer__i sprite-img sp-sprite sp-sprite__shotme-grey"></i>
+                        <a href="http://shotme.shablonkin.shn-host.ru/">
+                            <i class="footer__i sprite-img sp-sprite sp-sprite__shotme-grey"></i>
+                        </a>
                     </div>
                 </div>
             </div>
