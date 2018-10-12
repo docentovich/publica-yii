6e206d034f4be4158f340a043efb139093d6b2ec
Andrey Prozorov <andrey.prozorov@firstlinesoftware.com>
2018-10-12 19:26:32 +0300

    (bug fix) yii needs JQ 224

--- app/templates/frontend/tosee/layouts/main.php
+++ app/templates/frontend/tosee/layouts/main.php
@@ -155,7 +155,10 @@ $this->beginPage(); ?>
       </div>
     </div>
     <!-- CDN-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+    <script
+            src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+            crossorigin="anonymous"></script>
     <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
     <!-- --CDN---->
     <?php $this->endBody() ?>
