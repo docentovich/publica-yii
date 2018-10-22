ef26c2daa56275401531dfd3ffbd8e1d6475675f
docen <docentovich@gmail.com>
2018-10-22 09:52:08 +0300

    (bug fix) reconfigure configs so projects modules not being included in console app

--- app/common/config/bootstrap.php
+++ app/common/config/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
-require_once "bootstrap-projects-constants.php";
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@modules', dirname(dirname(__DIR__)) . '/src/modules');
+Yii::setAlias('@app', dirname(dirname(__DIR__)) . '/src');
 Yii::setAlias('@models', dirname(dirname(__DIR__)) . '/src/models');
 Yii::setAlias('@components', dirname(dirname(__DIR__)) . '/src/components');
 Yii::setAlias('@templates', dirname(dirname(__DIR__)) . '/src/templates');
--- app/frontend/config/bootstrap.php
+++ app/frontend/config/bootstrap.php
@@ -1,1 +1,19 @@
 <?php
+define("TOSEE", 1);
+define("TOSEE_DEV", "tosee.loc");
+define("TOSEE_PROD", "tosee.shablonkin.shn-host.ru");
+
+define("PROBANK", 2);
+define("PROBANK_DEV", "probank.loc");
+define("PROBANK_PROD", "publicayii-probank.shablonkin.shn-host.ru");
+
+switch ($_SERVER['SERVER_NAME']) {
+    case  TOSEE_DEV:
+    case  TOSEE_PROD:
+        define("PROJECT", TOSEE);
+        break;
+    case PROBANK_DEV:
+    case PROBANK_PROD :
+        define("PROJECT", PROBANK);
+        break;
+}
