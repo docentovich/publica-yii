dae8ad557e69a499bb9d2fec9cee6a9a9de62cad
Andrey Prozorov <andrey.prozorov@firstlinesoftware.com>
2018-10-22 19:10:06 +0300

    (develop) login page

--- app/src/templates/backend/main/BackendAsset.php
+++ app/src/templates/backend/main/BackendAsset.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Andrey
- * Date: 23.05.2017
- * Time: 15:08
- */
+
 
 namespace app\assets;
 use yii\web\AssetBundle;
@@ -13,12 +8,12 @@ class BackendAsset extends AssetBundle
 {
     public $sourcePath = '@current_template/assets';
     public $css = [
-        'css/vendor.css',
         'css/main.css',
+        'bundle/vendor.css',
     ];
     public $js = [
-        'js/vendor.js',
         'js/main.js',
+        'bundle/vendor.js',
     ];
     public $jsOptions = ['position' => \yii\web\View::POS_BEGIN];
     public $depends = [
