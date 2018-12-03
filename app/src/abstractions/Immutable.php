6f13e16c0dfee029396b52a0f10cadaf9c22d564
docen <docentovich@gmail.com>
2018-12-03 12:18:52 +0300

    (develop) immutable can store closure

---
+++ app/src/abstractions/Immutable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\abstractions;
+
+class Immutable implements \app\interfaces\dto
+{
+    protected $_params = [];
+
+    public function __construct($params)
+    {
+        $this->_params = $params;
+    }
+
+    public function __get($key)
+    {
+        if (isset($this->_params[$key])) {
+            return $this->_params[$key];
+        } else {
+            return null;
+        }
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (isset($this->_params[$name])) {
+            return $this->_params[$name](...$arguments);
+        } else {
+            throw new \Exception('method not found');
+        }
+    }
+
+    public function __set($key, $val)
+    {
+        if (!isset($this->_params[$key])) {
+            $this->_params[$key] = $val;
+        } else {
+            return;
+        }
+    }
+}
