d1483416324944b9868572a385c6fcdcd02df052
docen <docentovich@gmail.com>
2018-12-12 08:56:24 +0300

    search

---
+++ app/src/widgets/header/assets/js/additional.js
@@ -0,0 +1,20 @@
+//http://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+$('#search-input').on('keyup', debounce(function(){
+    var $this = $(this);
+    $.post('/')
+},500));
