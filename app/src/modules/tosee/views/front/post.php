abc18b9cad0c3954a6a54a874a3d004acd78e2f7
a.prozorov <andrey.prozorov@firstlinesoftware.com>
2018-12-12 21:21:49 +0300

    share

--- app/src/modules/tosee/views/front/post.php
+++ app/src/modules/tosee/views/front/post.php
@@ -78,7 +78,10 @@ use yii\widgets\Pjax;
                                 <?= (\Yii::$app->user->can('user')) ? 'like-action' : ''; ?>"></i>
                         <i class="icon-share"></i>
                         <i class="icon-buy"></i>
-                        <button class="button" data-sharer="somesharer" data-width="800" data-height="600" data-title="Checkout Sharer.js!" data-url="https://ellisonleao.github.io/sharer.js/">Share!</button>
+                        <button class="button" data-sharer="somesharer" data-width="800" data-height="600"
+                                data-title="Checkout Sharer.js!" data-url="https://ellisonleao.github.io/sharer.js/">
+                            Share!
+                        </button>
                     </div>
                 </div>
             </div>
@@ -112,14 +115,15 @@ use yii\widgets\Pjax;
                             <?php } ?>
                         </div>
 
-                        <div class="comment-form">
-                            <?php $new_comment = new \app\models\Comments(); ?>
-                            <?php $comment_form = \yii\widgets\ActiveForm::begin([
-                                'action' => '/comment',
-                                'id' => "comment-form-{$image->id}",
-                                'options' => [
-                                    'class' => 'form form-ajax',
-                                    'onsuccess' => "function(data) {
+                        <?php if (\Yii::$app->user->can('user')) { ?>
+                            <div class="comment-form">
+                                <?php $new_comment = new \app\models\Comments(); ?>
+                                <?php $comment_form = \yii\widgets\ActiveForm::begin([
+                                    'action' => '/comment',
+                                    'id' => "comment-form-{$image->id}",
+                                    'options' => [
+                                        'class' => 'form form-ajax',
+                                        'onsuccess' => "function(data) {
                                             var comment = $('<div class=\"comment new-comment\" style=\'display: none\'>
                                                             <div class=\"comment-avatar\">
                                                                 <img src=\'' + data.comment.user.profile.avatar_url + '\'/>
@@ -132,28 +136,29 @@ use yii\widgets\Pjax;
                                             $('#comments-text-{$image->id}').val('');
                                             comment.slideDown(\"slow\");
                                     }",
-                                    'onerror' => "function(data) {
+                                        'onerror' => "function(data) {
                                      }"
-                                ],
-                                'enableClientValidation' => true,
-                            ]); ?>
-                            <div class="comment-text">
-                                <?= $comment_form->field($new_comment, 'text')
-                                    ->textarea(['id' => "comments-text-{$image->id}"])
+                                    ],
+                                    'enableClientValidation' => true,
+                                ]); ?>
+                                <div class="comment-text">
+                                    <?= $comment_form->field($new_comment, 'text')
+                                        ->textarea(['id' => "comments-text-{$image->id}"])
+                                        ->label(false); ?>
+                                </div>
+
+                                <?= $comment_form->field($new_comment, 'image_id', [
+                                    'template' => "{input}",
+                                    "options" => ['tag' => false]
+                                ])
+                                    ->hiddenInput(['value' => $image->id])
                                     ->label(false); ?>
+
+
+                                <?= \yii\helpers\Html::submitButton(\Yii::t('app/tosee', 'Comment')); ?>
+                                <?php \yii\widgets\ActiveForm::end(); ?>
                             </div>
-
-                            <?= $comment_form->field($new_comment, 'image_id', [
-                                'template' => "{input}",
-                                "options" => ['tag' => false]
-                            ])
-                                ->hiddenInput(['value' => $image->id])
-                                ->label(false); ?>
-
-
-                            <?= \yii\helpers\Html::submitButton(\Yii::t('app/tosee', 'Comment')); ?>
-                            <?php \yii\widgets\ActiveForm::end(); ?>
-                        </div>
+                        <?php } ?>
                     </div>
                     <!--// Comment-->
 
