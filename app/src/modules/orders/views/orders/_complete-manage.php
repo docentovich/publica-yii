d4470f194f218ef6eafe98c1684cf16312ff9369
docen <docentovich@gmail.com>
2019-01-11 11:53:38 +0300

    intermediate

--- app/src/modules/orders/views/orders/_complete-manage.php
+++ app/src/modules/orders/views/orders/_complete-manage.php
@@ -3,3 +3,30 @@
  * @var \orders\dto\OrdersTransportModel $orderTransportModel
  * @var \yii\web\View $this
  */
+?>
+<div class="single-member order">
+    <div class="member-header order-header">
+        <div class="title">Анастасия</div>
+        <div class="sub-title">Модель</div>
+        <div class="order-header-avatars">
+            <div class="flex-el avatar" style="background-image:url(/images/my-avatar.jpg)"></div><i class="flex-el icon-exchange2" aria-hidden="true"></i>
+            <div class="flex-el avatar" style="background-image:url(/images/customer-avatar.jpg)"></div>
+        </div>
+        <div class="order-geo-time">
+            <div class="order-geo-time-inner">
+                <div class="flex-el"><i class="icon-geo"></i><span>Орел</span></div>
+                <div class="flex-el"><i class="icon-clock"></i><span>18.00 - 22.00</span></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="finish-order">
+    <div class="finish-block">
+        <label>Поставтье оценку (0-5 баллов) о пратнере</label>
+        <div id="rate"></div>
+    </div>
+    <div class="finish-block finish-comment">
+        <label>Оставтье окмментарий о партнере</label>
+        <textarea class="message"></textarea>
+    </div>
+</div>
--- app/src/modules/orders/views/orders/order-chat.php
+++ app/src/modules/orders/views/orders/order-chat.php
@@ -1,1 +1,36 @@
-<?php
+<div class="single-member order">
+    <div class="member-header order-header">
+        <div class="title">Анастасия</div>
+        <div class="sub-title">Модель</div>
+        <div class="order-header-avatars">
+            <div class="flex-el avatar" style="background-image:url(/images/my-avatar.jpg)"></div><i class="flex-el icon-exchange2" aria-hidden="true"></i>
+            <div class="flex-el avatar" style="background-image:url(/images/customer-avatar.jpg)"></div>
+        </div>
+        <div class="order-geo-time">
+            <div class="order-geo-time-inner">
+                <div class="flex-el"><i class="icon-geo"></i><span>Орел</span></div>
+                <div class="flex-el"><i class="icon-clock"></i><span>18.00 - 22.00</span></div>
+            </div>
+        </div>
+    </div>
+    <div class="member-body order-body">
+        <div class="order-body-inner">
+            <div class="comment">
+                <div class="comment-avatar"><img src="https://dummyimage.com/100x100/000/fff"/></div>
+                <div class="comment-description"><strong>user name</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</span></div>
+            </div>
+            <div class="comment">
+                <div class="comment-avatar"><img src="https://dummyimage.com/100x100/000/fff"/></div>
+                <div class="comment-description"><strong>user name</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</span></div>
+            </div>
+            <textarea class="message"></textarea>
+            <button class="green-button">Отрпавить сообщение</button>
+        </div>
+    </div>
+    <div class="bottom-bar"><span id="finish-order-toggle">Завершить заказ</span></div>
+    <div id="finish-order"><span>Завершить заказ?</span>
+        <div class="finish-order-control-row"><a class="finish-order-control" href="/finish-order.html" id="finish-order-control-yes">Да</a>
+            <button class="finish-order-control" id="finish-order-control-no">Нет</button>
+        </div>
+    </div>
+</div>
